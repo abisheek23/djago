2339af27cd60b98278781f155c0a7a588dcca220
abisheek23 <abisheek2397@gmail.com>
2024-11-05 11:35:00 +0530

    django

--- student_manag/app/views.py
+++ student_manag/app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render ,redirect
 
 # Create your views here.
-std=[]
+std=[{'roll_no':'1','name':'asd','age':'20'},{'roll_no':'2','name':'aws','age':'22'},{'roll_no':'3','name':'sws','age':'23'}]
 def home(req):
     if req.method=='POST':
         roll=req.POST['roll_no']
@@ -11,4 +11,30 @@ def home(req):
         return redirect(home)
     else:
         return render(req,'home.html',{'student':std})
+    
+def edit_std(req,a):
+    data=''
+    for i in std:
+        if i['roll_no']==a:
+            data=i
+    if req.method=='POST':
+        roll=req.POST['roll_no']
+        name=req.POST['name']
+        age=req.POST['age']
+        data['roll_no']=roll
+        data['name']=name
+        data['age']=age
+        return redirect(home)
+    else: 
+        return render(req,'edit.html',{'data':data})
+ 
+ 
+def delete(req,a):
+    for i in std:
+        if i['roll_no']==a:
+            std.remove(i)
+        return redirect(home)
+
+
+
         
